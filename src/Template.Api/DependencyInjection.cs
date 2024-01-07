c5ed81168fe121eb44cd07d097b6156facbe03e5
WorldWideWest <dzeno.dzafic@windowslive.com>
2024-01-07 12:20:54 +0100

    Registered configuration to di

--- src/Template.Api/DependencyInjection.cs
+++ src/Template.Api/DependencyInjection.cs
@@ -7,6 +7,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApi(this IServiceCollection services)
     {
         services.ConfigureSwagger().ConfigureApiVersioning();
+
         return services;
     }
 }
