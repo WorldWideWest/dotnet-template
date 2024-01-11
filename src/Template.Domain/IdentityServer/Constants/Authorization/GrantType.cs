fc6959bd9551b911cf2252c7ae304ad92d95cdf5
WorldWideWest <dzeno.dzafic@windowslive.com>
2024-01-11 16:51:33 +0100

    Removed unused code

--- src/Template.Domain/IdentityServer/Constants/Authorization/GrantType.cs
+++
@@ -1,8 +0,0 @@
-namespace Template.Domain.IdentityServer.Constants.Authorization;
-
-public static class GrantType
-{
-    public const string Password = "password";
-
-    public static IEnumerable<string> SupportedGrantTypes => new List<string> { Password };
-}
