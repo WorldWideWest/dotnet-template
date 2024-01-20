0ddaf4ea086e60b61c403141d52cfc0876032743
WorldWideWest <dzeno.dzafic@windowslive.com>
2024-01-20 09:13:17 +0100

    Creating scripts and Dockerfile to make generating certificates automatically and starting up .net from the script instead of the command

--- compose/nginx/startup.sh
+++ compose/nginx/startup.sh
@@ -5,11 +5,13 @@ mkdir -p certs
 
 openssl genpkey \
     -algorithm RSA \
-    -out keys/localhost.key
+    -out /etc/ssl/private/localhost.key
 
 openssl req \
-    -new -x509 -key keys/localhost.key \
-    -out certs/localhost.crt \
+    -new -x509 -key /etc/ssl/private/localhost.key \
+    -out /etc/ssl/certs/localhost.crt \
     -days 365 \
     -subj "/CN=localhost" \
     -addext "subjectAltName=DNS:localhost"
+
+exec nginx -g 'daemon off;'
