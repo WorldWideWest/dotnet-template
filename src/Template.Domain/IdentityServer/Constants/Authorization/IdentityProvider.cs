4eac29c91b2d3cac8e8041737724ba332d97068d
WorldWideWest <dzeno.dzafic@windowslive.com>
2024-03-06 11:47:59 +0100

    Rebuilding on change with develop.watch

--- src/Template.Domain/IdentityServer/Constants/Authorization/IdentityProvider.cs
+++ src/Template.Domain/IdentityServer/Constants/Authorization/IdentityProvider.cs
@@ -3,5 +3,5 @@ namespace Template.Domain.IdentityServer.Constants.Authorization;
 public static class IdentityProvider
 {
     public const string Google = "Google";
-    public const string Local = "local";
+    public const string Local = "ocal";
 }
