6a627951e2c3da7b5549067d889bdcbd05358981
WorldWideWest <dzeno.dzafic@windowslive.com>
2024-03-06 14:03:55 +0100

    Updating api scope naming scheme for profile password

--- src/Template.Domain/Identity/Constants/Authorization/Policy.cs
+++ src/Template.Domain/Identity/Constants/Authorization/Policy.cs
@@ -6,5 +6,5 @@ public static class Policy
     public const string Write = "write_access";
     public const string Update = "update_access";
     public const string Delete = "delete_access";
-    public const string UpdatePassword = "update_password_access";
+    public const string UpdatePassword = "update_profile_password_access";
 }
--- src/Template.Domain/IdentityServer/Constants/Authorization/ApiScope.cs
+++ src/Template.Domain/IdentityServer/Constants/Authorization/ApiScope.cs
@@ -5,7 +5,8 @@ public static class ApiScope
     public const string Read = $"{ApiResource.Template}_read";
     public const string Write = $"{ApiResource.Template}_write";
     public const string Update = $"{ApiResource.Template}_update";
-    public const string UpdatePassword = $"{ApiResource.Template}_update_password";
     public const string Delete = $"{ApiResource.Template}_delete";
     public const string Test = $"{ApiResource.Template}_test";
+
+    public const string UpdatePassword = $"{ApiResource.Template}_update_profile_password";
 }
